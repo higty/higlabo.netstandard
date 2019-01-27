9991f99715dbc39f00ea72b5e958958b4035b32b
higty <higuchihiguchihiguchi@hotmail.com>
2019-01-27 13:05:00 +0900

    Update Nuget package.

---
+++ NugetPackage/UploadPackage20190127_130412.cmd
@@ -0,0 +1,3 @@
+nuget push C:\GitHub\higty\HigLabo.NetStandard\HigLabo.Mail\bin\Release\HigLabo.Mail.2.0.0.6.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+nuget push C:\GitHub\higty\HigLabo.NetStandard\HigLabo.Mapper\bin\Release\HigLabo.Mapper.2.0.0.1.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey f3053919-51ca-4158-bedd-467327b3afd3
+pause
